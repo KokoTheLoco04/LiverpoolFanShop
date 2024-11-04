defae50b29ad812a3e6330755b3451518c92f3c4
KokoTheLoco04 <kaloyanzahariev04@gmail.com>
2024-11-04 17:44:21 +0200

    Add project files.

---
+++ LiverpoolFanShop/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LiverpoolFanShop.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
