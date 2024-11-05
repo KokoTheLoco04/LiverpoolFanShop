fe65d2a7cc5799409aa79b12e1f9d04d27cf0b85
KokoTheLoco04 <kaloyanzahariev04@gmail.com>
2024-11-05 18:46:21 +0200

    Initial Migration made

--- LiverpoolFanShop/Program.cs
+++ LiverpoolFanShop/Program.cs
@@ -13,7 +13,6 @@ builder.Services.AddApplicationServices();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseMigrationsEndPoint();
@@ -21,7 +20,6 @@ if (app.Environment.IsDevelopment())
 else
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
