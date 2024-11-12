0208c64569139169f1b770783363b79f25703418
KokoTheLoco04 <kaloyanzahariev04@gmail.com>
2024-11-12 18:50:05 +0200

    Layout and index razor pages updated for Admin

---
+++ LiverpoolFanShop/Areas/Admin/Controllers/ProductsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LiverpoolFanShop.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductsController : AdminBaseController
+    {
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        public IActionResult Edit() 
+        {
+            return View();
+        }
+    }
+}
